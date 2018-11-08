999f4b815229fc1e6bf2db79c0fb3af343ee06a8
Olga Allen <olyaallen@gmail.com>
2018-11-08 15:54:16 -0700

    first

---
+++ src/Layout/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Routing from '../Routing.js'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+           <Routing/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Layout/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Olga Allen</h1>
+         <br/>
+        <h1>Full Stack React Web Developer</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
